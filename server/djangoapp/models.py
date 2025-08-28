e259f6fb2ce72b3921858af2bdb18b03329f2b57
stefanroman22 <stefanromanpers@gmail.com>
2025-08-28 05:35:57 -0400

    Update app

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,33 @@
-# Uncomment the following imports before adding the Model code
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+class CarMake(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    created_at = models.DateTimeField(default=now)
 
+    def __str__(self):
+        return self.name
 
-# Create your models here.
+class CarModel(models.Model):
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    HATCHBACK = 'Hatchback'
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+    CAR_TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (HATCHBACK, 'Hatchback'),
+    ]
 
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE, related_name='models')
+    name = models.CharField(max_length=50)
+    car_type = models.CharField(max_length=20, choices=CAR_TYPE_CHOICES)
+    year = models.IntegerField(validators=[MinValueValidator(2015), MaxValueValidator(2023)])
+    created_at = models.DateTimeField(default=now)
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+    def __str__(self):
+        return f"{self.make.name} - {self.name} ({self.year})"
