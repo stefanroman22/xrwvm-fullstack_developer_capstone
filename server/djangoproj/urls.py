0cab65514afee0960e0c853b00d99980bbbedf23
stefanroman22 <80206920+stefanroman22@users.noreply.github.com>
2025-08-28 11:43:01 +0300

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-from django.conf.urls.static import static
+from django.views.static import serve
 from django.conf import settings
+from django.conf.urls.static import static
+import os  # Import os for path operations
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,6 +30,20 @@ urlpatterns = [
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
-    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
-    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path(
+        'dealer/<int:dealer_id>/',
+        TemplateView.as_view(template_name="index.html")
+        ),
+    path(
+        'postreview/<int:dealer_id>/',
+        TemplateView.as_view(template_name="index.html")
+        ),
+    # Serve the manifest.json file
+    re_path(
+        r'^manifest.json$',
+        serve,
+        {'document_root': os.path.join(
+            settings.BASE_DIR, 'frontend/build'),
+            'path': 'manifest.json'}
+    ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
